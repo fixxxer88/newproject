4bdda3e094e8e4afe3e7a01476c7a8898877bbf8
fix <fixxxer88@gmail.com>
2019-08-19 10:57:49 +0300

    ugalzhopa

---
+++ new.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// var leftBorderWidth = 1;
+// {let second = 2;
+//     console.log(second);
+// }
+const pi = 3.14;
+
+let jook = {
+    legNumber: 11,
+    name: "John",
+    isAsshole: true
+};
+// console.log("jooks number of legs is " +jook.legNumber);
+// console.log("jooks name is "+jook["name"]);
+// let bassStringName = ['','G','D','A','E','B']
+
+// console.log (bassStringName[5]);
+//alert ("ZATKNIS SUKA");
+// let result = +prompt ("who is jook?", "MUDAK!!!1");
+// console.log (typeof(result));
+let incr = 5,
+    decr = 5;
+
+
+console.log(pi);
+console.log(decr--);
+console.log(decr);
+let isShave = (decr == 4);
+
+console.log("is asshole " + jook.isAsshole);
+console.log("is shave " + isShave);
+console.log(!jook.isAsshole || !isShave);
